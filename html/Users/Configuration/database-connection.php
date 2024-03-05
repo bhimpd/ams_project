d8f327abcd6344ff9a27a35e4367d0ffd0098844
Bhim Prasad Lamichhane <dreamypd73@gmail.com>
2024-03-06 03:54:40 +0545

    db config changed

--- html/Users/Configuration/database-connection.php
+++ html/Users/Configuration/database-connection.php
@@ -11,16 +11,16 @@ class DBConnect
   private $hostname;
   private $username;
   private $password;
-  private $database = "mainams"; //"ams"
+  private $database = "ams"; //"ams"
 
   public $conn;
 
   //begins connection on object instantiation
   public function __construct()
   {
-    $this->hostname = "localhost";  //"amsdb";
-    $this->username = "root";    //"sanchay";
-    $this->password = "";  //"sanchay";
+    $this->hostname = "amsdb";  //"amsdb";
+    $this->username = "sanchay";    //"sanchay";
+    $this->password = "sanchay";  //"sanchay";
     $this->connectToDatabase();
   }
 
